3784e13bb0ccacff7d76596f45bb2a99bc3d5d6d
Kevin Staunton-Lambert <kevstal@meta.com>
2023-05-27 05:04:24 +1000

    Kevs wrapper

--- kjsl_run.sh
+++ kjsl_run.sh
@@ -1,8 +1,8 @@
 # Kev's build script
-HERE=$PWD
+HERE=$HOME
 
 IMAGE_STATE=$(docker images -q bash2py:latest 2> /dev/null)
-RUN_STATE=$(docker ps -qf "ancestor=bask2py")
+RUN_STATE=$(docker ps -qf "ancestor=bash2py")
 
 create_docker_image () {
   if [[ "$IMAGE_STATE" == "" ]]; then
@@ -13,15 +13,16 @@ create_docker_image () {
 run_docker_container () {
   if [[ "$RUN_STATE" == "" ]]; then
     echo "Spinning up new bash2py container"
-    docker run --rm --name bash2py --cap-add sys_ptrace --mac-address="de:ad:be:ef:00:03" -p127.0.0.1:2322:22 -d -v "$HERE:/host" bash2py
+    docker run --rm --name bash2py --cap-add sys_ptrace -d -v "$HERE:/host" -i bash2py
     sleep 2
 
     # update running state image id to be the newly created Docker container
     RUN_STATE=$(docker ps -qf "ancestor=bash2py")
-
-    ssh-keygen -f "$HOME/.ssh/known_hosts" -R [localhost]:2322
   fi
 
   echo "Rejoining bash2py container $RUN_STATE"
   docker exec -i -t "$RUN_STATE" /bin/bash
 }
+
+create_docker_image
+run_docker_container
